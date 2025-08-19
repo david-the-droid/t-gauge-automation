fc49b99daa7925b95dc4716e8af8cf99cfc185ab
david-the-droid <54676795+david-the-droid@users.noreply.github.com>
2025-08-19 12:53:52 +0100

    Tweaks after verifying script with shell checker

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -3,10 +3,11 @@
 sudo apt install python3-venv
 sudo apt install python3-pip
 
-mkdir ~/t_gauge_automation && cd ~/t_gauge_automation
+mkdir ~/t_gauge_automation
+cd ~/t_gauge_automation || exit 1
 
 python3 -m venv venv
 
-source .venv/bin/activate
+.venv/bin/activate
 
 python3 -m pip install -r requirements.txt
