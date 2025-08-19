0035bbdbe17485ea138d1fe02867d92204b92ee0
david-the-droid <54676795+david-the-droid@users.noreply.github.com>
2025-08-19 12:51:10 +0100

    Update create_venv.sh to generate and configure a venv

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-# empty shell script - intention is for this to configure a venv on a Linux OS system
+
+sudo apt install python3-venv
+sudo apt install python3-pip
+
+mkdir ~/t_gauge_automation && cd ~/t_gauge_automation
+
+python3 -m venv venv
+
+source .venv/bin/activate
+
+python3 -m pip install -r requirements.txt
