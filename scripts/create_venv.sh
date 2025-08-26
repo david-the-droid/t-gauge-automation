b490d0884851c15711107f0d780bd2f1e154a961
david-the-droid <54676795+david-the-droid@users.noreply.github.com>
2025-08-26 21:34:11 +0100

    Simplifying script, removing unnecessary installs

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-sudo apt install python3-venv
-sudo apt install python3-pip
-
-mkdir ~/t_gauge_automation
-cd ~/t_gauge_automation || exit 1
-
 python3 -m venv venv
 
 .venv/bin/activate
