5a85cbea23feaff0e4a3247b2b06d09bb5f6087d
david-the-droid <mayalsdavid@gmail.com>
2025-09-10 21:12:06 +0100

    Dummy commit to test PAT token is securely stored

--- main_app/main_app.py
+++ main_app/main_app.py
@@ -1,1 +1,25 @@
 # empty python file
+
+from gpiozero import PWMOutputDevice
+from time import sleep
+
+# pwm controller
+
+# GPIO 27 (13): AIN2
+# GPIO 22 (15): AIN1
+
+# GPIO18, f=40Hz
+pwm1 = PWMOutputDevice(pin=27, frequency=140)
+pwm2 = PWMOutputDevice(pin=22, frequency=140)
+
+# duty cycle 50%
+try: 
+	pwm1.value = 0
+	pwm2.value = 0.5
+except keyBoardInterrupt:
+	pwm.off()
+	print("PWM stopped")
+	
+
+while True:
+	sleep(1)
