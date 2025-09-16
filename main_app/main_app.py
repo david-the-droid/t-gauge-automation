751f0750de7ce1b69e8a09d6e00ab02b1da2c8bf
david-the-droid <mayalsdavid@gmail.com>
2025-09-16 17:00:09 +0100

    Adding basic pwm script as starting point

--- main_app/main_app.py
+++ main_app/main_app.py
@@ -1,1 +1,17 @@
-# empty python file
+from gpiozero import PWMOutputDevice
+from time import sleep
+
+pwm1 = PWMOutputDevice(27, frequency=137)  # GPIO 27
+pwm2 = PWMOutputDevice(22, frequency=137)  # GPIO 22
+
+try: 
+    pwm1.value = 0
+    pwm2.value = 0.3
+
+    while True:
+        sleep(1)
+
+except KeyboardInterrupt:
+    print("\nProgram interrupted by user. Stopping train...")
+    pwm1.off()
+    pwm2.off()
